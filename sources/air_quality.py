4ad80ba71fe1a3deb3010d5477663b482bf920f3
dominikjagoda <74588679+dominikjagoda@users.noreply.github.com>
2022-11-06 20:26:34 +0100

    Update air_quality.py

--- sources/air_quality.py
+++ sources/air_quality.py
@@ -5,7 +5,7 @@ from typing import Dict, Any
 from datetime import datetime, date, timedelta
 
 url = "https://api.apilayer.com/exchangerates_data/timeseries?"
-headers = {"apikey": "wsv9UWyKU8QBv1B8C5U2ej6Q9hnnv05I"}
+
 payload = {
         }
 try:
