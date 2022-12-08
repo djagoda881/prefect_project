1ed0117553e78a30eac5feddb76ca86a62aec67c
djagoda881 <djagoda@dyvenia.com>
2022-12-08 13:20:41 +0100

    Added github block

--- deployment.py
+++ deployment.py
@@ -1,13 +1,15 @@
 from ex_rates_task import ex_rates_to_postgresql
 from prefect.deployments import Deployment
-from prefect.blocks.core import Block
-from prefect.filesystems import LocalFileSystem
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("githubdj")
 
 
 deployment = Deployment.build_from_flow(
     flow=ex_rates_to_postgresql,
     name="log-simple",
     work_queue_name="test",
+    storage=github_block,
 )
 
 if __name__ == "__main__":
