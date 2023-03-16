245a231a85d6cbb8ca6aae5c9e649a3daf265826
djagoda881 <djagoda881@gmail.com>
2023-03-16 15:05:00 +0100

    ✨ Added deployment

--- deployment.py
+++ deployment.py
@@ -1,15 +1,15 @@
 from ex_rates_task import ex_rates_to_postgresql
 from prefect.deployments import Deployment
-from prefect.filesystems import GitHub
+from prefect_github.repository import GitHubRepository
 
-github_block = GitHub.load("githubdj")
+github_repository_block = GitHubRepository.load("git-repo")
 
 
 deployment = Deployment.build_from_flow(
     flow=ex_rates_to_postgresql,
     name="git-dominik",
     work_queue_name="test",
-    storage=github_block,
+    storage=github_repository_block,
 )
 
 if __name__ == "__main__":
